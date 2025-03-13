693abdb59a29f4789ac472c9fcdae450cc11c181
Wensi Xu <xuwensi0515@gmail.com>
2025-03-13 14:31:11 -0700

    [Bug] Resolve build issue

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,11 +1,16 @@
 // frontend/src/components/Dashboard.js
 import React from 'react';
+import PropTypes from 'prop-types';
 import { AppBar, Tabs, Tab, Box, Typography } from '@mui/material';
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
   return (
-    <div role="tabpanel" hidden={value !== index} {...other}>
+    <div 
+      role="tabpanel" 
+      hidden={value !== index} 
+      {...other}
+    >
       {value === index && (
         <Box p={3}>
           <Typography>{children}</Typography>
@@ -14,6 +19,12 @@ function TabPanel(props) {
     </div>
   );
 }
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  value: PropTypes.number.isRequired,
+  index: PropTypes.number.isRequired,
+};
 
 const Dashboard = () => {
   const [value, setValue] = React.useState(0);
