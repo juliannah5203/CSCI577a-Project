02145a1229e65c2f1876950d30862bc6bad34bf7
Sid-Jacob <jacobsid12138@gmail.com>
2025-04-03 22:13:23 -0700

    [Feature] Add docker support for backend

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -1,20 +1,23 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import pluginReact from "eslint-plugin-react";
-
+import { defineConfig } from "eslint/config";
 
 /** @type {import('eslint').Linter.Config[]} */
-export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { 
-    globals: {
-      // ...globals.browser,
-      ...globals.node  // Add Node.js globals (including process)
+export default defineConfig([
+  {
+    ignores: ['dist/', 'node_modules/', '*.min.js', 'build/', "eslint.config.mjs", "__test__/*.test.js"],
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    languageOptions: { 
+      globals: {
+        // ...globals.browser,
+        ...globals.node  // Add Node.js globals (including process)
+      },
+      sourceType: 'commonjs',
     },
-    // Allow CommonJS module syntax
-    sourceType: 'commonjs'
-  },},
+  },
   pluginJs.configs.recommended,
   pluginReact.configs.flat.recommended,
-  {ignores: ['dist/', 'node_modules/', '*.min.js', 'build/', "eslint.config.mjs"],},
-];
+]);
