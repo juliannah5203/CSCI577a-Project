ed0e8e87a7043c524563452358f8420a0b56a517
Wayne <wayneclub@Waynes-MacBook-Pro.local>
2025-04-14 23:16:49 -0700

    Resolve merge conflict in index.js match with React 18

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,11 @@
 // frontend/src/index.js
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
 
-// ReactDOM.render(
-//   <React.StrictMode>
-//       <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
+const container = document.getElementById("root");
+const root = createRoot(container);
 
-const container = document.getElementById('root');
-const root = ReactDOM.createRoot(container);
 root.render(
   <React.StrictMode>
     <App />
