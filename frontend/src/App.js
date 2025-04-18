33be424e9866394237ca9744e048f776c2e914bf
Wayne <wayneclub@Waynes-MacBook-Pro.local>
2025-04-18 01:32:19 -0700

    Fix snackbar and merge with last App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SignInPage from "./components/SignInPage";
 import Dashboard from "./components/Dashboard";
@@ -10,29 +10,85 @@ import CheckIn from "./components/CheckIn";
 import MoodTrends from "./components/MoodTrends";
 // import AIInsights from "./components/AIInsights"; // Uncomment if used
 import ProtectedRoute from "./components/ProtectedRoute";
-import { useStoreUserCookie } from './hooks/useCurrentUser';
+import { useStoreUserCookie } from "./hooks/useCurrentUser";
 import { SnackbarProvider } from "./context/SnackbarContext";
+import { setSnackbarHandler } from "./utils/axiosInstance";
+import { useSnackbar } from "./context/SnackbarContext";
+
+function App() {
+  const { showSnackbar } = useSnackbar();
+  useEffect(() => {
+    setSnackbarHandler((message, severity) => {
+      showSnackbar(message, severity);
+    });
+  }, [showSnackbar]);
+
+  useStoreUserCookie();
+
+  return (
+    <Routes>
+      <Route path="/" element={<SignInPage />} />
+      <Route
+        path="/dashboard"
+        element={
+          <ProtectedRoute>
+            <Dashboard />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/userprofile"
+        element={
+          <ProtectedRoute>
+            <UserProfile />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/checkin"
+        element={
+          <ProtectedRoute>
+            <CheckIn />
+          </ProtectedRoute>
+        }
+      />
+      {/* <Route path="/ai" element={<ProtectedRoute><AIInsights /></ProtectedRoute>} /> */}
+      <Route
+        path="/trends"
+        element={
+          <ProtectedRoute>
+            <Trends />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/history"
+        element={
+          <ProtectedRoute>
+            <History />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/mood"
+        element={
+          <ProtectedRoute>
+            <MoodTrends />
+          </ProtectedRoute>
+        }
+      />
+      {/* <Route path="/settings" element={<ProtectedRoute><Settings /></ProtectedRoute>} */}
+    </Routes>
+  );
+}
 
 function AppWrapper() {
-  useStoreUserCookie();
-  
   return (
     <SnackbarProvider>
       <Router>
-        <Routes>
-          <Route path="/" element={<SignInPage />} />
-          <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
-          <Route path="/userprofile" element={<ProtectedRoute><UserProfile /></ProtectedRoute>} />
-          <Route path="/checkin" element={<ProtectedRoute><CheckIn /></ProtectedRoute>} />
-          {/* <Route path="/ai" element={<ProtectedRoute><AIInsights /></ProtectedRoute>} /> */}
-          <Route path="/trends" element={<ProtectedRoute><Trends /></ProtectedRoute>} />
-          <Route path="/history" element={<ProtectedRoute><History /></ProtectedRoute>} />
-          <Route path="/mood" element={<ProtectedRoute><MoodTrends /></ProtectedRoute>} />
-          {/* <Route path="/settings" element={<ProtectedRoute><Settings /></ProtectedRoute>} */}
-        </Routes>
+        <App />
       </Router>
     </SnackbarProvider>
-
   );
 }
 
