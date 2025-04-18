e20f4885ad9ac11237e42b4dc723eabb0d92d619
Wayne <wayneclub@Waynes-MacBook-Pro.local>
2025-04-18 01:39:58 -0700

    Fix redefine root in index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,4 +1,3 @@
-// frontend/src/index.js
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
@@ -6,17 +5,8 @@ import App from "./App";
 const container = document.getElementById("root");
 const root = createRoot(container);
 
-// ReactDOM.render(
-//   <React.StrictMode>
-//       <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
-
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-
+  </React.StrictMode>
 );
